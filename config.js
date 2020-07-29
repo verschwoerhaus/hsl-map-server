1f4c7a9e087905c16a8b50aab3d6fc37594b5de9
Maximilian Richt <maxi@richt.name>
2020-07-29 16:09:49 +0200

    cleanup

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-const sourcesUrl = "hsl-map-server:8080/map/v1/";
-const glyphsUrl = `file://${process.env.WORK}/node_modules/hsl-map-style/`;
-
 module.exports = {
   "/map/v1/stop-map": {
     "source": `otpstops://${process.env.OTP_URL}`,
